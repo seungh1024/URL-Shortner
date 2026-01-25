c06afb82c4c0f91d666827f68ee0664cb86b7e55
seungh1024 <seungh1024@naver.com>
2026-01-26 02:43:11 +0900

    chore: setting project

--- src/test/java/com/shortener/url_shortener/UrlShortenerApplicationTests.java
+++ src/test/java/com/shortener/url_shortener/UrlShortenerApplicationTests.java
@@ -1,9 +1,7 @@
 package com.shortener.url_shortener;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class UrlShortenerApplicationTests {
 
 	@Test
